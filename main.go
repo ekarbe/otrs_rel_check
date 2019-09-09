c0a4ec5e6e87b814bdcdc2bbd89cefde06d999a6
Eike Christian Karbe <eichkarbe@yahoo.de>
2019-09-09 16:31:52 +0200

    refactoring

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
@@ -16,8 +17,8 @@ type otrs struct {
 	date    string
 }
 
-var help = flag.Bool("help", false, "prints the help")
-var major = flag.Int64("major", 0, "defines the version to be checked. defaults to all versions.")
+var h = flag.Bool("h", false, "prints the help")
+var v = flag.Int64("v", 0, "defines the version to be checked. defaults to all versions.")
 var t = flag.Int("t", 0, "defines the time of a new version")
 
 func init() {
@@ -25,12 +26,43 @@ func init() {
 }
 
 func main() {
-	if *help {
+	if *h {
 		PrintHelp()
+		os.Exit(0)
 	}
-	resp, _ := http.Get("https://ftp.otrs.org/pub/otrs/")
+	body, err := GetBody()
+	if err != nil {
+		print(err)
+	}
+	releases, err := GetReleases(body)
+	if err != nil {
+		print(err)
+	}
+	if *t != 0 {
+		releases, err = GetTimeWindowReleases(releases)
+		if err != nil {
+			print(err)
+		}
+	}
+	fmt.Print(releases)
+}
+
+//GetBody ...
+func GetBody() (string, error) {
+	resp, err := http.Get("https://ftp.otrs.org/pub/otrs/")
+	if err != nil {
+		return "", err
+	}
 	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+//GetReleases ...
+func GetReleases(body string) (map[string]otrs, error) {
 	entries := strings.Split(string(body), "</tr>")
 	version := regexp.MustCompile("([0-9]+\\.[0-9]+\\.[0-9]+)")
 	date := regexp.MustCompile("([0-9])([0-9]*-[0-9]*-[0-9]*) [0-9]*:[0-9]*")
@@ -41,23 +73,32 @@ func main() {
 		o.date = date.FindString(entries[i])
 		if o.version != "" && o.date != "" {
 			temp := string(o.version[0])
-			majorVersion, _ := strconv.ParseInt(temp, 10, 64)
-			if *major == 0 || majorVersion == *major {
+			majorVersion, err := strconv.ParseInt(temp, 10, 64)
+			if err != nil {
+				return nil, err
+			}
+			if *v == 0 || majorVersion == *v {
 				releases[o.version] = o
 			}
 		}
 	}
-	if *t != 0 {
-		timeWindow := time.Now().AddDate(0, 0, -*t)
-		for key, otrs := range releases {
-			layout := "2006-01-02 15:04"
-			parseTime, _ := time.Parse(layout, otrs.date)
-			if parseTime.Sub(timeWindow) < 0 {
-				delete(releases, key)
-			}
+	return releases, nil
+}
+
+//GetTimeWindowReleases ...
+func GetTimeWindowReleases(releases map[string]otrs) (map[string]otrs, error) {
+	timeWindow := time.Now().AddDate(0, 0, -*t)
+	for key, otrs := range releases {
+		layout := "2006-01-02 15:04"
+		parseTime, err := time.Parse(layout, otrs.date)
+		if err != nil {
+			return releases, err
+		}
+		if parseTime.Sub(timeWindow) < 0 {
+			delete(releases, key)
 		}
 	}
-	fmt.Println(releases)
+	return releases, nil
 }
 
 //PrintHelp ...
